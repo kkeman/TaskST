2524795a7ff085d114063a2f04718e5614e8e011
kkeman <kkeman11@gmail.com>
2022-04-25 21:49:12 +0900

    gradle, java update

--- app/src/main/java/com/service/codingtest/repository/DbImagePostRepository.kt
+++ app/src/main/java/com/service/codingtest/repository/DbImagePostRepository.kt
@@ -1,11 +1,13 @@
 package com.service.codingtest.repository
 
+import androidx.paging.ExperimentalPagingApi
 import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import com.service.codingtest.db.AppDB
 import com.service.codingtest.network.ImageAPI
 
 class DbImagePostRepository(val db: AppDB, private val imageAPI: ImageAPI) : ImageRepository {
+    @OptIn(ExperimentalPagingApi::class)
     override fun postsOfSubDocument(
         query: String,
         pageSize: Int
